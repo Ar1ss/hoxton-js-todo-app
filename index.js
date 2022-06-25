7003f9412218688173f458765edb02e652c5a617
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-06-25 13:53:49 +0200

    Added some functions

--- index.js
+++ index.js
@@ -3,14 +3,57 @@
 // - Create action functions that update state
 // - Create render functions that read from state
 
+
+let state = {
+  todos: [
+    {title : "Go shoping" , completed: false},
+    {title : "Work out"  , completed: false},
+    {title : "Eat diner" , completed: false}, 
+  ],
+  showCompleted:false
+}
+
+function incompleteTodo(){
+  return state.todos.filter(todo => todo.completed === false)
+}
+
+function creaeTodo(text){
+  let foundMatch = state.todos.some(todo => todo.text === text)
+  if (foundMatch) return
+}
+
+function completedTodos(){
+  return state.todos.filter(todo => todo.completed === true)
+}
+
+function deleteTodo(text){
+  let updatedTodos = state.todos.filter(todo => todo.text !== text)
+  state.todos = updatedTodos
+}
+
+function toggleTodo(text){
+  let match = state.todos.find(todo => todo.text === text)
+  if (!match) return
+}
+
+function toggleCompletedTodo(){
+  state.showCompleted = !state.showCompleted
+}
+
+
 function render() {
   document.body.textContent = ""
 
   let mainForm = document.createElement("form")
   mainForm.className = 'main-form'
+  
+  //Optinions
 
   let optnionsTitleh1 = document.createElement("h1")
   optnionsTitleh1.textContent = "OPTININOS"
+
+  let optinionsSection = document.createElement("section")
+  optinionsSection.className = "optinions__section"
 
   let optionsCheckBox = document.createElement("input")
   optionsCheckBox.id = "show-completed"
@@ -20,15 +63,29 @@ function render() {
   let optionsCheckBoxName = document.createElement("label")
   optionsCheckBoxName.setAttribute("for", "show-completed")
   optionsCheckBoxName.textContent = "Show completed"
-  
+
+  //Add item
+
   let addItemTitle = document.createElement('h1')
   addItemTitle.textContent = 'ADD ITEM'
+
+  let addItemSection = document.createElement("section")
+  addItemSection.className = "additem__section"
 
   let addItemInput = document.createElement('input')
   addItemInput.className = 'add-item__input'
   addItemInput.setAttribute('type' , 'text')
-  addItemInput.setAttribute('placeholder' , 'Add')
+  addItemInput.setAttribute('placeholder' , 'Add here...')
+
+  let addItemButton = document.createElement("button")
+  addItemButton.className = "additem__button"
+  addItemButton.textContent = "Add"
   
+  //Todo
+
+  let todoSection = document.createElement("section")
+  todoSection.className = "todo__section"
+
   let todoTitle = document.createElement('h1')
   todoTitle.textContent = 'TODO'
 
@@ -36,24 +93,26 @@ function render() {
   todoCheckBox.id = "todo-checkbox"
   todoCheckBox.setAttribute("type" , "checkbox")
 
-  let todoCheckBoxName = document.createElement('label')
-  todoCheckBoxName.setAttribute('for' , 'todo-checkbox')
-  todoCheckBoxName.textContent = 'I have to do'
+
+  //Completed
 
   let completedTitle = document.createElement('h1')
   completedTitle.textContent = 'COMPLETED'
 
+  optinionsSection.append(optionsCheckBox,optionsCheckBoxName)
+  addItemSection.append(addItemInput,addItemButton)
+  todoSection.append(todoCheckBox,)
+
   document.body.append(mainForm)
   mainForm.append(
     optnionsTitleh1,
-    optionsCheckBox,
-    optionsCheckBoxName,
+    optinionsSection,
     addItemTitle,
-    addItemInput,
+    addItemSection,
     todoTitle,
-    todoCheckBox,
-    todoCheckBoxName,
-    completedTitle)
+    todoSection,
+    completedTitle,
+    )
   
   
 }
